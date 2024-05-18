b10fc107473ba702a8a13bdb59a628e47ba833ce
Junior1511 <yunior122000@gmail.com>
2024-05-17 21:53:47 -0500

    Done for now, ill work on extra credit some other day, my brain is fried

--- javascript.js
+++ javascript.js
@@ -2,12 +2,18 @@ const btn = document.querySelector("#btn")
 const btn2 = document.querySelector("#ChangeGrid")
 const reset = document.querySelector("#reset")
 const container = document.querySelector("#container")
-const black = document.querySelector("#blackBtn")
-const rainbow = document.querySelector("#rainbowBtn")
+// const black = document.querySelector("#blackBtn")
+// const rainbow = document.querySelector("#rainbowBtn")
+// const yeahidk =  document.querySelector(".makeDivBlack")
+function eraseAll (){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+}
 
 function makeOne (){
     const div = document.createElement("div");
-    div.classList.add("grid")
+    div.setAttribute("style", `height: 31.25px; width :31.25px;`)
     div.addEventListener("mouseover", function(e){
         div.classList.add("makeDivBlack")
     })
@@ -15,13 +21,40 @@ function makeOne (){
 }
 
 btn.addEventListener("click",function(e){
+    eraseAll()
     for (i = 0; i < 256; i++) {
         makeOne()
     }
 })
 
-reset.addEventListener("click",function(e){
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-      }
-    })
+btn2.addEventListener("click",function(e){
+    eraseAll()
+    let num = prompt ("How big do you want the grid to be? (2 - 100)")
+    let sizeOfDiv = 500/num
+    for (i = 0; i < (num**2); i++) {
+        const div = document.createElement("div");
+        div.setAttribute("style", `height:${sizeOfDiv}px; width : ${sizeOfDiv}px`)
+        div.addEventListener("mouseover", function(e){
+            div.classList.add("makeDivBlack")
+        })
+        container.appendChild(div)
+    }
+})
+
+reset.addEventListener("click", eraseAll)
+
+// extra credit
+
+// rainbow.addEventListener("click",function(e){
+//     while (yeahidk.classList.contains("makeDivBlack")) {
+//         yeahidk.classList.remove("makeDivBlack");
+//         yeahidk.classList.add("makeDivRainbow")
+//       }
+// })
+
+// black.addEventListener("click",function(e){
+//     while (yeahidk.classList.contains("makeDivRainbow")) {
+//         yeahidk.classList.remove("makeDivRainbow");
+//         yeahidk.classList.add("makeDivBlack")
+//       }
+// })
